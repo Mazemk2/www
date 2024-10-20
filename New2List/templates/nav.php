a84a8d9585cf2fb645186573258b8bd78fb7c0a0
Mazemk2 <Mazemk2@googlemail.com>
2024-10-20 13:58:30 +0000

    to much do count

--- New2List/templates/nav.php
+++ New2List/templates/nav.php
@@ -1,6 +1,6 @@
 <!-- nav.php -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><?php echo htmlspecialchars($brandTitle); ?></a>
+    <a class="navbar-brand" href="./Bootstraptestone.html"><?php echo htmlspecialchars($brandTitle); ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
