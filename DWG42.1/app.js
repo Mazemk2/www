c13988581203d28a846cc3f46c22e91e6bc07b89
Mazemk2 <Mazemk2@googlemail.com>
2024-10-23 11:53:41 +0000

    there sould be an server

--- DWG42.1/app.js
+++ DWG42.1/app.js
@@ -2,25 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// EJS als View Engine festlegen
+// EJS als View Engine konfigurieren
 app.set('view engine', 'ejs');
 
 // Statische Dateien bereitstellen
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routen definieren
 app.get('/', (req, res) => {
-    res.render('index');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+    res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', { title: 'Contact' });
+});
+
+// Server starten
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
 });
