571de8ab7949a89ac9228a4b2f3f859561f1655c
Mazemk2 <Mazemk2@googlemail.com>
2024-10-30 07:04:42 +0000

    app changes added Header/ first version of a Todolist

--- react-react212-mkirchner/src/App.js
+++ react-react212-mkirchner/src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 import LikeButton from './LikeButton';
 import TodoList from './TodoList';
 
+// Erstellen einer einfachen React-Komponente in JSX
+function Header() {
+  return <h1>Develop. Preview. Ship.</h1>;
+  }
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
@@ -19,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <TodoList />
       </header>
     </div>
   );
--- react_02/react-test-mkirchner/src/App.js
+++ react_02/react-test-mkirchner/src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 import LikeButton from './LikeButton';
 import TodoList from './TodoList';
 
+// Erstellen einer einfachen React-Komponente in JSX
+function Header() {
+  return <h1>Develop. Preview. Ship.</h1>;
+  }
+
+
 function App() {
   return (
+    
     <div className="App">
+      <div Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
@@ -19,6 +27,7 @@ function App() {
         >
           Learn React
         </a>
+        <TodoList />
       </header>
     </div>
   );
